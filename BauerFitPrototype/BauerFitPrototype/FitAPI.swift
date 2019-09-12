f81af6d5e2b416c280de0a5ff67e9d45681fe6de
Eray Diler <eray.diler@gmail.com>
2019-09-12 15:01:24 +0300

    Cocoapods error fixes.
    Fixed build errors caused by cocoapods magpie library.

---
+++ BauerFitPrototype/BauerFitPrototype/FitAPI.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  BauerFitPrototype
+//
+//  Created by Eray on 12.09.2019.
+//  Copyright © 2019 Hipo. All rights reserved.
+//
+
+import Magpie
+
+class FitAPI: Magpie {
+    
+}
